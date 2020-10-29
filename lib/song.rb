940195e4c88861d6b152ad974ebbd57f00c0f8c7
Maria Castaneda <mariamcastaneda29@gmail.com>
2020-10-29 19:20:17 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -16,4 +16,49 @@ class Song
     song
   end
 
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find do |song|
+      song.name == name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical 
+    @@all.sort_by do |song|
+      song.name
+    end
+  end
+
+  def self.new_from_filename(filename)
+# "Taylor Swift - Blank Space.mp3"
+    file = filename.split(/- | .mp3/)
+    file.collect do |word|
+      word.strip
+    end
+    song = self.create
+
+    song.artist_name = file[0]
+    song.name = find[1]
+
+  end
+
+
+
+
+
 end
